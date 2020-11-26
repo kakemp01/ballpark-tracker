a1e150afd0a1da450ff011fa2bad4956058adb78
Karsten Kempe <kakemp@microsoft.com>
2020-11-26 08:40:24 +0000

    add new comment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // app.use(express.json());
 
-// Hello!
+// fix the problem here!
 
 //main app page
 app.get('/', (req, res) => res.render('index', { AZURE_MAPS_KEY: process.env.AZURE_MAPS_KEY }));
